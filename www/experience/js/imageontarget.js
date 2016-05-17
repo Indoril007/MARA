8e37a884f1158216f1bb34069d2e42ca02c36921
Fu Hsien <fuhsienng@gmail.com>
2016-05-17 11:48:28 +1000

    added ecse lab oscilloscope

--- www/experience/js/imageontarget.js
+++ www/experience/js/imageontarget.js
@@ -11,7 +11,7 @@ var World = {
 			Important: If you replace the tracker file with your own, make sure to change the target name accordingly.
 			Use a specific target name to respond only to a certain target or use a wildcard to respond to any or a certain group of targets.
 		*/
-		this.tracker = new AR.ClientTracker("assets/MARA.wtc", {
+		this.tracker = new AR.ClientTracker("assets/MARA_v4.wtc", {
 			onLoaded: this.worldLoaded
 		});
 
@@ -34,80 +34,43 @@ var World = {
 		//Augmentation of "Tutorial" button and name of equipment
 		var Tutorial = new AR.ImageDrawable(imgTute, 0.25, {
 			offsetX: -0.45,
-			offsetY: 0.65,
+			offsetY: -0.65,
 			onClick: function() {
 			//load tutorial contents here
 			}
 		});
 
-		var Eq_name = new AR.ImageDrawable(imgName_FG, 0.4, {
-			offsetX: -0.45,
-			offsetY: -0.6			
-		});
+		var Eq_name_FG = new AR.ImageDrawable(imgName_FG, 0.4, {		offsetX: -0.45,offsetY: 0.6	});
 
 
 		// Column 1 Button 1
-		var c1b1 = new AR.ImageDrawable(imgOne, 0.05, {
-			offsetX: -0.511,
-			offsetY: 0.235,
+		var c1b1 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.511,offsetY: 0.235,
 			//onClick: function() {
 			//AR.context.openInBrowser("http://www.monash.edu/");
 			//}
 		});
 
-		var c1b2 = new AR.ImageDrawable(imgOne, 0.05, {
-			offsetX: -0.511,
-			offsetY: 0.133,
-		});
+		var c1b2 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.511,offsetY: 0.133		});
 
-		var c1b3 = new AR.ImageDrawable(imgOne, 0.05, {
-			offsetX: -0.511,
-			offsetY: 0.031,
-		});
+		var c1b3 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.511,offsetY: 0.031		});
 
-		var c1b4 = new AR.ImageDrawable(imgOne, 0.05, {
-			offsetX: -0.511,
-			offsetY: -0.071,
-		});
+		var c1b4 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.511,offsetY: -0.071		});
 
-		var c1b5 = new AR.ImageDrawable(imgOne, 0.05, {
-			offsetX: -0.511,
-			offsetY: -0.173,
-		});
+		var c1b5 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.511,offsetY: -0.173		});
 
-		var c2b1 = new AR.ImageDrawable(imgOne, 0.07, {
-			offsetX: -0.324,
-			offsetY: 0.35
-		});
+		var c2b1 = new AR.ImageDrawable(imgOne, 0.07, {			offsetX: -0.324,offsetY: 0.35		});
 
-		var c2b2 = new AR.ImageDrawable(imgOne, 0.07, {
-			offsetX: -0.324,
-			offsetY: 0.258
-		});
+		var c2b2 = new AR.ImageDrawable(imgOne, 0.07, {			offsetX: -0.324,offsetY: 0.258		});
 
-		var c2b3 = new AR.ImageDrawable(imgOne, 0.07, {
-			offsetX: -0.324,
-			offsetY: 0.166
-		});
-		var c2b4 = new AR.ImageDrawable(imgOne, 0.07, {
-			offsetX: -0.324,
-			offsetY: 0.076
-		});
+		var c2b3 = new AR.ImageDrawable(imgOne, 0.07, {			offsetX: -0.324,offsetY: 0.166		});
 
-		var c2b5 = new AR.ImageDrawable(imgOne, 0.07, {
-			offsetX: -0.324,
-			offsetY: -0.013
-		});
+		var c2b4 = new AR.ImageDrawable(imgOne, 0.07, {			offsetX: -0.324,offsetY: 0.076		});
 
-		var c2b6 = new AR.ImageDrawable(imgOne, 0.07, {
-			offsetX: -0.324,
-			offsetY: -0.102
-		});
+		var c2b5 = new AR.ImageDrawable(imgOne, 0.07, {			offsetX: -0.324,offsetY: -0.013		});
 
-		var round_button = new AR.ImageDrawable(imgRound, 0.1, {
-			offsetX: -0.511,
-			offsetY: 0.337
-		})
+		var c2b6 = new AR.ImageDrawable(imgOne, 0.07, {			offsetX: -0.324,offsetY: -0.102		});
+
+		var round_button = new AR.ImageDrawable(imgRound, 0.1, {			offsetX: -0.511,offsetY: 0.337		});
 
 		/*
 			The last line combines everything by creating an AR.Trackable2DObject with the previously created tracker, the name of the image target and the drawable that should augment the recognized image.
@@ -115,57 +78,78 @@ var World = {
 		*/
 		var ECSE_func_gen = new AR.Trackable2DObject(this.tracker, "function generator half", {
 			drawables: {
-				cam: [Tutorial,Eq_name,c1b1,c1b2,c1b3,c1b4,c1b5, c2b1,c2b2,c2b3,c2b4,c2b5,c2b6, round_button]
+				cam: [Tutorial,Eq_name_FG,c1b1,c1b2,c1b3,c1b4,c1b5, c2b1,c2b2,c2b3,c2b4,c2b5,c2b6, round_button]
 			}
 		});
 		//====================================================End of Function Generator ECSE Lab=======================================================================================
 
+
 		//=======================================================Oscilloscope ENG1002==================================================================================================
 		/*
 			The AR.Trackable2DObject for the second page uses the same tracker but with a different target name and the second overlay.
-		*/
+
+		*/	
+		var Eq_name_Os = new AR.ImageDrawable(imgName_Os, 0.4, {		offsetX: -0.45,offsetY: 0.65	});	
+
 		// Column 1 Button 1
-		var O1_c1b1 = new AR.ImageDrawable(imgOne, 0.06, {
-			offsetX: -0.426,
-			offsetY: 0.2445,
-		});
+		// O1 = Oscilloscope 1002
+		var O1_c1b1 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.426,offsetY: 0.2445		});
 
-		var O1_c1b2 = new AR.ImageDrawable(imgOne, 0.06, {
-			offsetX: -0.426,
-			offsetY: 0.138,
-		});
+		var O1_c1b2 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.426,offsetY: 0.138		});
 
-		var O1_c1b3 = new AR.ImageDrawable(imgOne, 0.06, {
-			offsetX: -0.426,
-			offsetY: 0.0315,
-		});
+		var O1_c1b3 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.426,offsetY: 0.0315		});
 
-		var O1_c1b4 = new AR.ImageDrawable(imgOne, 0.06, {
-			offsetX: -0.426,
-			offsetY: -0.075,
-		});
+		var O1_c1b4 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.426,offsetY: -0.075		});
 
-		var O1_c1b5 = new AR.ImageDrawable(imgOne, 0.06, {
-			offsetX: -0.426,
-			offsetY: -0.1815,
-		});
+		var O1_c1b5 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.426,offsetY: -0.1815	});
 
-		var O1_round1 = new AR.ImageDrawable(imgRound, 0.1, {
-			offsetX: -0.42,
-			offsetY: -0.28,
-		});
+		var O1_round1 = new AR.ImageDrawable(imgRound, 0.1, {			offsetX: -0.42,offsetY: -0.28	});
 
-		var O1_mult_purpose_knob = new AR.ImageDrawable(imgRound, 0.25, {
-			offsetX: -0.224,
-			offsetY: 0.328,
-		});
+		var O1_mult_purpose_knob = new AR.ImageDrawable(imgRound, 0.25, {			offsetX: -0.224,offsetY: 0.328		});
 
-		var ENG1002_osci = new AR.Trackable2DObject(this.tracker, "Oscilloscope1", {
+		var ENG1002_osci = new AR.Trackable2DObject(this.tracker, "ENG1002_Oscilloscope", {
 			drawables: {
-				cam: [O1_c1b1, O1_c1b2, O1_c1b3, O1_c1b4, O1_c1b5, O1_round1,O1_mult_purpose_knob]
-				//cam: [c1b1,c1b2,c1b3,c1b4,c1b5, c2b1,c2b2,c2b3,c2b4,c2b5,c2b6, round_button]
+				cam: [Tutorial,Eq_name_Os,O1_c1b1, O1_c1b2, O1_c1b3, O1_c1b4, O1_c1b5, O1_round1,O1_mult_purpose_knob]
 			}
 		});
+		//====================================================End of Oscilloscope ENG1002=============================================================================================
+
+		//=======================================================Oscilloscope ECSE Lab=================================================================================================
+				// Column 1 Button 1
+		// OL = Oscilloscope Lab 
+
+		// LEFT COLUMN BUTTONS
+		var OL_c1b1 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.6,offsetY: 0.2795		});
+
+		var OL_c1b2 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.6,offsetY: 0.171		});
+
+		var OL_c1b3 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.6,offsetY: 0.0635		});
+
+		var OL_c1b4 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.6,offsetY: -0.044		});
+
+		var OL_c1b5 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.6,offsetY: -0.1505	});
+
+		var OL_round1 = new AR.ImageDrawable(imgRound, 0.08, {			offsetX: -0.6,offsetY: -0.253	});
+
+		// TOP PART BUTTONS
+		var OL_mult_purpose_knob = new AR.ImageDrawable(imgRound, 0.2, {			offsetX: -0.377,offsetY: 0.383		});
+
+		var OL_r1b1 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.2214,offsetY: 0.433		});
+
+		var OL_r2b1 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.2214,offsetY: 0.338		});
+
+		/*
+			The last line combines everything by creating an AR.Trackable2DObject with the previously created tracker, the name of the image target and the drawable that should augment the recognized image.
+			Please note that in this case the target name is a wildcard. Wildcards can be used to respond to any target defined in the target collection. If you want to respond to a certain target only for a particular AR.Trackable2DObject simply provide the target name as specified in the target collection.
+		*/
+		var ECSE_func_gen = new AR.Trackable2DObject(this.tracker, "Lab_Oscilloscope", {
+			drawables: {
+				cam: [Tutorial,Eq_name_Os,OL_c1b1, OL_c1b2, OL_c1b3, OL_c1b4, OL_c1b5, OL_round1,OL_mult_purpose_knob, OL_r1b1, OL_r2b1]
+			}
+		});
+		//==========================================================End of Oscilloscope ECSE Lab=======================================================================================
+
+
 
 	},
 
