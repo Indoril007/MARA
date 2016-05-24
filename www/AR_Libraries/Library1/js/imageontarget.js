1d0dcf2a3bbc718fc6857b0cfc2582ba7e10f2cb
James <jdbor1@student.monash.edu>
2016-05-24 17:26:01 +1000

    more progress on tutorials

--- www/AR_Libraries/Library1/js/imageontarget.js
+++ www/AR_Libraries/Library1/js/imageontarget.js
@@ -1,12 +1,24 @@
+var GLOBAL_DEBUG_FLAG = true;
+
 var World = {
 	loaded: false,
-	tracked_devices: null,
+	tracked_devices: [],
+	tracked_tutorialSteps: [],
+	tutorial_stepIndex: 0,
 	
 	init: function initFn() {
+		if (GLOBAL_DEBUG_FLAG === true) {
+			console.log("===========World===========");
+			console.log("init");
+		} 
 		this.createOverlays();
 	},
 
 	createOverlays: function createOverlaysFn() {
+		if (GLOBAL_DEBUG_FLAG === true) {
+			console.log("===========World===========");
+			console.log("createOverlays");
+		} 
 		/*
 			First an AR.ClientTracker needs to be created in order to start the recognition engine. It is initialized with a URL specific to the target collection. Optional parameters are passed as object in the last argument. In this case a callback function for the onLoaded trigger is set. Once the tracker is fully loaded the function worldLoaded() is called.
 			Important: If you replace the tracker file with your own, make sure to change the target name accordingly.
@@ -20,7 +32,6 @@ var World = {
 			The next step is to create the augmentation. In this example an image resource is created and passed to the AR.ImageDrawable. A drawable is a visual component that can be connected to an IR target (AR.Trackable2DObject) or a geolocated object (AR.GeoObject). The AR.ImageDrawable is initialized by the image and its size. Optional parameters allow for position it relative to the recognized target.
 		*/
 		
-		this.tracked_devices = [];
 		var button_drawables = [];
 		
 		// for (i = 0; i < devices.length; i++) {
@@ -72,28 +83,72 @@ var World = {
 	},
 
 	loadTutorial: function(device_key, tutorial_key) {
+		if (GLOBAL_DEBUG_FLAG === true) {
+			console.log("===========World===========");
+			console.log("loadTutorial");
+			console.log("device_key: " + device_key);
+			console.log("tutorial_key: " + tutorial_key);
+		} 
+		
 		this.disableTrackedDevices();
 		var tutorial = devices[device_key].tutorials[tutorial_key];
+		var tutorialSteps = tutorial.steps;
 		
-		console.log("GOT THIS FAR===================================================");
-		console.log(tutorial_key);
+		for (i = 0; i < tutorialSteps.length; i++) {
+			var step_buttons = tutorialSteps[i].buttons;
+			var button_drawables = []; 
+			 
+			for (j = 0; j < step_buttons.length; j++) {
+				button_drawables.push(step_buttons[j].getARImageDrawable());
+			}
+			
+			this.tracked_tutorialSteps[i] = new AR.Trackable2DObject(this.tracker, devices[device_key].name, {
+				drawables: {
+					cam: button_drawables,
+				},
+				enabled: false,
+			});
+			
+			button_drawables = []; 
+		}	
 		
+		$(window).bind("click", function(event, ui) {
+			console.log("SWIPE REGISTERED ============================================================");
+			console.log(World.tracked_tutorialSteps);
+			console.log(World.tutorial_stepIndex);
+			(World.tracked_tutorialSteps[World.tutorial_stepIndex]).enabled = false;
+			World.tutorial_stepIndex += 1;
+			(World.tracked_tutorialSteps[World.tutorial_stepIndex]).enabled = true;
+		})
 		
+		this.tracked_tutorialSteps[0].enabled = true;
 	},
 	
 	disableTrackedDevices: function() {
+		if (GLOBAL_DEBUG_FLAG === true) {
+			console.log("===========World===========");
+			console.log("disableTrackedDevices");
+		} 
 		for (i = 0; i < (this.tracked_devices).length; i++) {
 			(this.tracked_devices[i]).enabled = false;
 		}
 	},
 	
 	enableTrackedDevices: function() {
+		if (GLOBAL_DEBUG_FLAG === true) {
+			console.log("===========World===========");
+			console.log("enableTrackedDevices");
+		} 
 		for (i = 0; i < (this.tracked_devices).length; i++) {
 			(this.tracked_devices[i]).enabled = true;
 		}
 	},
 	
 	worldLoaded: function worldLoadedFn() {
+		if (GLOBAL_DEBUG_FLAG === true) {
+			console.log("===========World===========");
+			console.log("worldLoaded");
+		} 
 		var cssDivLeft = " style='display: table-cell;vertical-align: middle; text-align: right; width: 50%; padding-right: 15px;'";
 		var cssDivRight = " style='display: table-cell;vertical-align: middle; text-align: left;'";
 		var cssFont = " style='display: table-cell;vertical-align: middle; text-align: middle;'";
@@ -112,3 +167,4 @@ var World = {
 };
 
 World.init();
+
