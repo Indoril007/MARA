532529507acfe9eb3941e2d6dfd823058883324f
Fu Hsien <fuhsienng@gmail.com>
2016-05-24 18:15:20 +1000

    Lab equipment

--- www/experience/js/imageontarget.js
+++ www/experience/js/imageontarget.js
@@ -43,35 +43,101 @@ var World = {
 
 		var Eq_name_FG = new AR.ImageDrawable(imgName_FG, 0.4, {		offsetX: -0.45,offsetY: 0.6	});
 
+		//Vertical 1 cm = 0.081037		100pixel = 0.10941
+		//Horizontal 1 cm = 0.068965 	100pixel = 0.10951, 0.10016
 
-		// Column 1 Button 1
+
+
+		// Column 1 
+		var round_button = new AR.ImageDrawable(imgRound, 0.1, {			offsetX: -0.511,offsetY: 0.337		});
+
 		var c1b1 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.511,offsetY: 0.235,
 			//onClick: function() {
 			//AR.context.openInBrowser("http://www.monash.edu/");
 			//}
 		});
 
-		var c1b2 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.511,offsetY: 0.133		});
+		var c1b2 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.5179,offsetY: 0.133			});
 
-		var c1b3 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.511,offsetY: 0.031		});
+		var c1b3 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.5179,offsetY: 0.0317		});
 
-		var c1b4 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.511,offsetY: -0.071		});
+		var c1b4 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.5179,offsetY: -0.0696		});
 
-		var c1b5 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.511,offsetY: -0.173		});
+		var c1b5 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.5179,offsetY: -0.1709		});
 
-		var c2b1 = new AR.ImageDrawable(imgOne, 0.07, {			offsetX: -0.324,offsetY: 0.35		});
+		// Column 2 Function
 
-		var c2b2 = new AR.ImageDrawable(imgOne, 0.07, {			offsetX: -0.324,offsetY: 0.258		});
+		var c2b1 = new AR.ImageDrawable(imgOne, 0.07, {			offsetX: -0.32,offsetY: 0.35			});
 
-		var c2b3 = new AR.ImageDrawable(imgOne, 0.07, {			offsetX: -0.324,offsetY: 0.166		});
+		var c2b2 = new AR.ImageDrawable(imgOne, 0.07, {			offsetX: -0.32,offsetY: 0.2575			});
 
-		var c2b4 = new AR.ImageDrawable(imgOne, 0.07, {			offsetX: -0.324,offsetY: 0.076		});
+		var c2b3 = new AR.ImageDrawable(imgOne, 0.07, {			offsetX: -0.32,offsetY: 0.1651			});
 
-		var c2b5 = new AR.ImageDrawable(imgOne, 0.07, {			offsetX: -0.324,offsetY: -0.013		});
+		var c2b4 = new AR.ImageDrawable(imgOne, 0.07, {			offsetX: -0.32,offsetY: 0.0726			});
 
-		var c2b6 = new AR.ImageDrawable(imgOne, 0.07, {			offsetX: -0.324,offsetY: -0.102		});
+		var c2b5 = new AR.ImageDrawable(imgOne, 0.07, {			offsetX: -0.32,offsetY: -0.0198			});
 
-		var round_button = new AR.ImageDrawable(imgRound, 0.1, {			offsetX: -0.511,offsetY: 0.337		});
+		var c2b6 = new AR.ImageDrawable(imgOne, 0.07, {			offsetX: -0.32,offsetY: -0.1099			});
+
+		var c2b7 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.32,offsetY: -0.2215			});
+
+		// Row 1 - run mode
+
+		var r1b1 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.1915,offsetY: 0.35			});
+
+		var r1b2 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.0761,offsetY: 0.35			});
+
+		var r1b3 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: 0.0392,offsetY: 0.35			});
+
+		var r1b4 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: 0.1546,offsetY: 0.35			});
+
+		//Row 2
+
+		var r2b1 = new AR.ImageDrawable(imgOne, 0.07, {			offsetX: -0.1353,offsetY: 0.258			});
+
+		var r2b2 = new AR.ImageDrawable(imgOne, 0.07, {			offsetX: 0.0316,offsetY: 0.258			});
+
+		var r2b3_knob = new AR.ImageDrawable(imgRound, 0.35, {			offsetX: 0.3641,offsetY: 0.2958			});
+
+		//Row 3
+
+		var r3b1 = new AR.ImageDrawable(imgOne, 0.07, {			offsetX: -0.1353,offsetY: 0.166			});
+
+		var r3b2 = new AR.ImageDrawable(imgOne, 0.07, {			offsetX: 0.0316,offsetY: 0.166			});		
+
+		var r3b3 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: 0.2426,offsetY: 0.166			});
+
+		var r3b4 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: 0.4616,offsetY: 0.166			});	
+
+		//Vertical 1 cm = 0.081037		100pixel = 0.10941
+		//Horizontal 1 cm = 0.068965 	100pixel = 0.10951, 0.10016
+
+		//Row 4
+
+		var r4b1 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.198,offsetY: 0.076			});
+
+		var r4b2 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.0855,offsetY: 0.076			});	
+
+		//Row 5
+
+		var r5b1 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.198,offsetY: -0.013			});
+
+		var r5b2 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.0855,offsetY: -0.013		});	
+
+		var r5b3 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: 0.0261,offsetY: -0.013			});	
+
+		//Row 6
+
+		var r6b1 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.198,offsetY: -0.1099		});
+
+		var r6b2 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.0855,offsetY: -0.1099		});	
+
+		var r6b3 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: 0.0261,offsetY: -0.1099		});	
+
+		// key pad
+		var keypad = new AR.ImageDrawable(imgRound, 0.5, {			offsetX: 0.2794,offsetY: -0.0386			});
+
+
 
 		/*
 			The last line combines everything by creating an AR.Trackable2DObject with the previously created tracker, the name of the image target and the drawable that should augment the recognized image.
@@ -79,10 +145,148 @@ var World = {
 		*/
 		var ECSE_func_gen = new AR.Trackable2DObject(this.tracker, "function generator half", {
 			drawables: {
-				cam: [Tutorial,Eq_name_FG,c1b1,c1b2,c1b3,c1b4,c1b5, c2b1,c2b2,c2b3,c2b4,c2b5,c2b6, round_button]
+				cam: [Tutorial,Eq_name_FG,
+					round_button,c1b1,c1b2,c1b3,c1b4,c1b5, 
+					c2b1,c2b2,c2b3,c2b4,c2b5,c2b6,c2b7,
+					r1b1, r1b2, r1b3, r1b4, 
+					r2b1, r2b2, r2b3_knob,
+					r3b1, r3b2, r3b3, r3b4,
+					r4b1, r4b2,
+					r5b1, r5b2,
+					r6b1, r6b2, r6b3,
+					keypad
+					]
 			}
 		});
 		//====================================================End of Function Generator ECSE Lab=======================================================================================
+
+		//=======================================================Oscilloscope ECSE Lab=================================================================================================
+		var Eq_name_Os = new AR.ImageDrawable(imgName_Os, 0.4, {		offsetX: -0.45,offsetY: 0.65	});	
+
+		/*
+		Measurements! delete when finished!
+		horizontal 1 cm = 0.103733333333
+		vertical 1cm = 0.101
+		*/
+
+		// OL = Oscilloscope Lab 
+		// LEFT COLUMN BUTTONS
+		var OL_c1b1 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.6,offsetY: 0.2795		});
+
+		var OL_c1b2 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.6,offsetY: 0.171		});
+
+		var OL_c1b3 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.6,offsetY: 0.0635		});
+
+		var OL_c1b4 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.6,offsetY: -0.044		});
+
+		var OL_c1b5 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.6,offsetY: -0.1505	});
+
+		var OL_round1 = new AR.ImageDrawable(imgRound, 0.08, {			offsetX: -0.6,offsetY: -0.253	});
+
+		// TOP PART BUTTONS
+		var OL_mult_purpose_knob = new AR.ImageDrawable(imgRound, 0.2, {			offsetX: -0.377,offsetY: 0.383		});
+
+		var OL_r1b1 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.2214,offsetY: 0.433		});
+
+		var OL_r1b2 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.0969,offsetY: 0.433		});	
+
+		var OL_r1b3 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: 0.0276,offsetY: 0.433		});	
+
+		var OL_r1b4 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: 0.152,offsetY: 0.433		});
+
+		var OL_r1b5 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: 0.2765,offsetY: 0.433		});
+
+		var OL_r1b6 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: 0.4010,offsetY: 0.433		});
+
+		//row 2
+
+		var OL_r2b1 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.2214,offsetY: 0.338		});
+
+		var OL_r2b2 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.0969,offsetY: 0.338		});	
+
+		var OL_r2b3 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: 0.0276,offsetY: 0.338		});	
+
+		var OL_r2b4 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: 0.152,offsetY: 0.338		});
+
+		var OL_r2b5 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: 0.2765,offsetY: 0.338		});
+
+		var OL_r2b6 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: 0.4010,offsetY: 0.338		});
+
+		var OL_run_stop = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: 0.5307,offsetY: 0.383		});
+
+		//row 3
+
+		var OL_r3b1 = new AR.ImageDrawable(imgRound, 0.1, {			offsetX: -0.3874,offsetY: 0.2117		});	
+
+		var OL_r3b2 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.2214,offsetY: 0.2117	});	
+
+		var OL_r3b3 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.0969,offsetY: 0.2117	});	
+
+		var OL_r3b4 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: 0.0246,offsetY: 0.2117		});	
+
+		var OL_r3b5_knob = new AR.ImageDrawable(imgRound, 0.12, {			offsetX: 0.1906,offsetY: 0.2066		});
+
+		var OL_r3b6 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: 0.3358,offsetY: 0.2117		});	
+
+		var OL_r3b7 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: 0.5225,offsetY: 0.2117		});
+
+		//row 4
+
+		var OL_r4b1 = new AR.ImageDrawable(imgRound, 0.12, {			offsetX: -0.3978,offsetY: 0.0653		});	
+
+		var OL_r4b2 = new AR.ImageDrawable(imgRound, 0.12, {			offsetX: -0.1644,offsetY: 0.0653		});	
+
+		var OL_r4b3 = new AR.ImageDrawable(imgRound, 0.12, {			offsetX: 0.069,offsetY: 0.0653			});	
+
+		var OL_r4b4 = new AR.ImageDrawable(imgRound, 0.12, {			offsetX: 0.3024,offsetY: 0.0653			});	
+
+		var OL_r4b5 = new AR.ImageDrawable(imgRound, 0.12, {			offsetX: 0.5385,offsetY: 0.0602			});
+
+		//row 5
+
+		var OL_r5b1 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.3874,offsetY: -0.0408		});	
+
+		var OL_r5b2 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.2759,offsetY: -0.0408		});	
+
+		var OL_r5b3 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: -0.1644,offsetY: -0.0408		});	
+
+		var OL_r5b4 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: 0.059,offsetY: -0.0408			});	
+
+		var OL_r5b5 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: 0.292,offsetY: -0.0458			});	
+
+		var OL_r5b6 = new AR.ImageDrawable(imgOne, 0.05, {			offsetX: 0.528,offsetY: -0.0509			});
+
+		//row 6
+
+		var OL_r6b1 = new AR.ImageDrawable(imgRound, 0.2, {			offsetX: -0.3978,offsetY: -0.1923		});	
+
+		var OL_r6b2 = new AR.ImageDrawable(imgRound, 0.2, {			offsetX: -0.1644,offsetY: -0.1923		});	
+
+		var OL_r6b3 = new AR.ImageDrawable(imgRound, 0.2, {			offsetX: 0.069,offsetY: -0.1923			});	
+
+		var OL_r6b4 = new AR.ImageDrawable(imgRound, 0.2, {			offsetX: 0.3024,offsetY: -0.1973			});	
+
+		var OL_r6b5 = new AR.ImageDrawable(imgRound, 0.2, {			offsetX: 0.5385,offsetY: -0.2117		});
+
+
+		/*
+			The last line combines everything by creating an AR.Trackable2DObject with the previously created tracker, the name of the image target and the drawable that should augment the recognized image.
+			Please note that in this case the target name is a wildcard. Wildcards can be used to respond to any target defined in the target collection. If you want to respond to a certain target only for a particular AR.Trackable2DObject simply provide the target name as specified in the target collection.
+		*/
+		var ECSE_func_gen = new AR.Trackable2DObject(this.tracker, "Lab_Oscilloscope", {
+			drawables: {
+				cam: [Tutorial,Eq_name_Os,
+					OL_c1b1, OL_c1b2, OL_c1b3, OL_c1b4, OL_c1b5, OL_round1,
+					OL_mult_purpose_knob, OL_r1b1, OL_r1b2, OL_r1b3, OL_r1b4, OL_r1b5, OL_r1b6, 
+					OL_r2b1, OL_r2b2, OL_r2b3, OL_r2b4, OL_r2b5, OL_r2b6, OL_run_stop,
+					OL_r3b1, OL_r3b2, OL_r3b3, OL_r3b4, OL_r3b5_knob, OL_r3b6, OL_r3b7,
+					OL_r4b1, OL_r4b2, OL_r4b3, OL_r4b4, OL_r4b5,
+					OL_r5b1, OL_r5b2, OL_r5b3, OL_r5b4, OL_r5b5, OL_r5b6,
+					OL_r6b1, OL_r6b2, OL_r6b3, OL_r6b4, OL_r6b5,
+					]
+			}
+		});
+		//====================================================================End of Oscilloscope ECSE Lab=============================================================================
 
 
 		//=======================================================Oscilloscope ENG1002==================================================================================================
@@ -90,7 +294,7 @@ var World = {
 			The AR.Trackable2DObject for the second page uses the same tracker but with a different target name and the second overlay.
 
 		*/	
-		var Eq_name_Os = new AR.ImageDrawable(imgName_Os, 0.4, {		offsetX: -0.45,offsetY: 0.65	});	
+		
 
 		// Column 1 Button 1
 		// O1 = Oscilloscope 1002
@@ -113,46 +317,10 @@ var World = {
 				cam: [Tutorial,Eq_name_Os,O1_c1b1, O1_c1b2, O1_c1b3, O1_c1b4, O1_c1b5, O1_round1,O1_mult_purpose_knob]
 			}
 		});
-		//====================================================End of Oscilloscope ENG1002=============================================================================================
-
-		//=======================================================Oscilloscope ECSE Lab=================================================================================================
-				// Column 1 Button 1
-		// OL = Oscilloscope Lab 
-
-		// LEFT COLUMN BUTTONS
-		var OL_c1b1 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.6,offsetY: 0.2795		});
-
-		var OL_c1b2 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.6,offsetY: 0.171		});
-
-		var OL_c1b3 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.6,offsetY: 0.0635		});
-
-		var OL_c1b4 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.6,offsetY: -0.044		});
-
-		var OL_c1b5 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.6,offsetY: -0.1505	});
-
-		var OL_round1 = new AR.ImageDrawable(imgRound, 0.08, {			offsetX: -0.6,offsetY: -0.253	});
-
-		// TOP PART BUTTONS
-		var OL_mult_purpose_knob = new AR.ImageDrawable(imgRound, 0.2, {			offsetX: -0.377,offsetY: 0.383		});
-
-		var OL_r1b1 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.2214,offsetY: 0.433		});
-
-		var OL_r2b1 = new AR.ImageDrawable(imgOne, 0.06, {			offsetX: -0.2214,offsetY: 0.338		});
-
-		/*
-			The last line combines everything by creating an AR.Trackable2DObject with the previously created tracker, the name of the image target and the drawable that should augment the recognized image.
-			Please note that in this case the target name is a wildcard. Wildcards can be used to respond to any target defined in the target collection. If you want to respond to a certain target only for a particular AR.Trackable2DObject simply provide the target name as specified in the target collection.
-		*/
-		var ECSE_func_gen = new AR.Trackable2DObject(this.tracker, "Lab_Oscilloscope", {
-			drawables: {
-				cam: [Tutorial,Eq_name_Os,OL_c1b1, OL_c1b2, OL_c1b3, OL_c1b4, OL_c1b5, OL_round1,OL_mult_purpose_knob, OL_r1b1, OL_r2b1]
-			}
-		});
-		//==========================================================End of Oscilloscope ECSE Lab=======================================================================================
-
+		//====================================================End of Oscilloscope ENG1002==============================================================================================
 
 		
-		//====================================Power Supply ENG1002==========================================================================================================================================
+		//==========================================================================Power Supply ENG1002===============================================================================
 		var ps_round1 = new AR.ImageDrawable(imgRound, 0.3, {
 			offsetX: -0.085,
 			offsetY: 0.26,
@@ -176,7 +344,7 @@ var World = {
 		}); 
 
 
-		//====================================End of Power Supply ENG1002=========================================================================================================================================	
+		//============================================================================End of Power Supply ENG1002========================================================================
 	},
 
 	worldLoaded: function worldLoadedFn() {
